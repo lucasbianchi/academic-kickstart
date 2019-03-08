5df3dbb1bd67a8f2e432dcd28589d3ba58b75de5
Lucas O. BIanchi <lbianchi@zoho.com>
2019-03-08 16:12:58 -0300

    Updating site vie mar  8 16:12:58 -03 2019

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo ""
+echo ""
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo ""
+echo ""
+
+echo "Uploading changes of root folder"
+git add .
+git commit -m "Updating site `date`"
+git push origin master
+
+echo ""
+echo ""
+echo "Uploading public folder"
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="Updating public folder `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
